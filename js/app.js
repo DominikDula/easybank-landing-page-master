a575292c98250a4d522c01e74ade398ce45d3de9
destrojer <dominodula@gmail.com>
2020-07-03 23:32:11 +0200

    mobile prototype v.01

---
+++ js/app.js
@@ -0,0 +1,34 @@
+
+
+let ham = document.querySelector(".ham");
+let closed = document.querySelector(".close");
+let navigation = document.querySelector(".navigation");
+console.log(ham,closed,navigation);
+
+
+
+
+function isActive(){
+    navigation.classList.add("active");
+    ham.style= "display:none;";
+    closed.style = "display:block;";
+    document.body.classList.add("bactive");
+    document.body.classList.remove("bclose");
+   
+}
+
+
+function nonActive(){
+    navigation.classList.remove("active");
+    ham.style= "display:block;";
+    closed.style = "display:none;";
+    document.body.classList.remove("bactive");
+    document.body.classList.add("bclose");
+   
+}
+
+ham.addEventListener("click",isActive);
+closed.addEventListener("click",nonActive);
+
+
+
